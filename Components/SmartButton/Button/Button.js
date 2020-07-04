5d7b38e5757554bc32282507b181dd9faa04f9e1
isberg1 <isberg1_github@outlook.com>
2020-07-04 22:11:38 +0200

    Moved click Animations to custom hook

--- Components/SmartButton/Button/Button.js
+++ Components/SmartButton/Button/Button.js
@@ -1,71 +1,38 @@
-import React, {useRef} from 'react';
-import {View, Text, TouchableOpacity, Animated, Easing} from 'react-native';
+import React, {useCallback} from 'react';
+import {View, Text, TouchableOpacity, Animated} from 'react-native';
+import {UseClickAnimation} from './useClickAnimations';
 import {styles} from './styles';
 
-export const Button = ({text, onPress, onLongPress, disabled, styling}) => {
-  const colorAnim = useRef(new Animated.Value(0)).current;
+export const Button = ({
+  text,
+  onPress = () => {},
+  onLongPress = () => {},
+  disabled,
+  styling,
+}) => {
+  const {
+    colorChangeStyling,
+    pressInAnimation,
+    pressOutAnimation,
+    longPressAnimation,
+  } = UseClickAnimation();
 
-  const longPressAnimation = callback =>
-    Animated.sequence([
-      Animated.timing(colorAnim, {
-        toValue: 2,
-        duration: 100,
-        useNativeDriver: false,
-      }),
-      Animated.timing(colorAnim, {
-        toValue: 1,
-        duration: 200,
-        useNativeDriver: false,
-      }),
-    ]).start(() => callback());
-
-  const pressInnAnimation = () =>
-    Animated.timing(colorAnim, {
-      toValue: 1,
-      duration: 200,
-      useNativeDriver: false,
-      easing: Easing.ease,
-    }).start();
-
-  const pressOutAnimation = () =>
-    Animated.timing(colorAnim, {
-      delay: 200,
-      toValue: 0,
-      duration: 200,
-      useNativeDriver: false,
-      easing: Easing.ease,
-    }).start();
-
-  const _onPress = () => {
-    !!onPress && onPress();
-  };
-
-  const _onLongPress = () => {
-    longPressAnimation(onLongPress ? onLongPress : () => {});
-  };
+  const _onPress = useCallback(() => onPress(), [onPress]);
+  const _onLongPress = useCallback(() => longPressAnimation(onLongPress), [
+    longPressAnimation,
+    onLongPress,
+  ]);
 
   return (
     <View
       style={[styles.buttonContainer, disabled && styles.disabled, styling]}>
-      <Animated.View
-        style={[
-          {
-            backgroundColor: colorAnim.interpolate({
-              inputRange: [0, 1, 2],
-              outputRange: [
-                'rgba(0,0,0, 0)',
-                'rgba(0,0,0,.7)',
-                'rgba(255, 0, 0, 0.7)',
-              ],
-            }),
-          },
-        ]}>
+      <Animated.View style={{backgroundColor: colorChangeStyling}}>
         <TouchableOpacity
-          activeOpacity={1}
+          activeOpacity={1} // disable default clickAnimation
           onPress={_onPress}
           disabled={disabled}
           delayLongPress={500}
-          onPressIn={pressInnAnimation}
+          onPressIn={pressInAnimation}
           onPressOut={pressOutAnimation}
           onLongPress={_onLongPress}>
           <Text style={[styles.text]}>{text}</Text>
---
+++ Components/SmartButton/Button/useClickAnimations.js
@@ -0,0 +1,63 @@
+import {useRef, useCallback, useMemo} from 'react';
+import {Animated, Easing} from 'react-native';
+
+export const UseClickAnimation = () => {
+  const colorAnim = useRef(new Animated.Value(0)).current;
+
+  const longPressAnimation = useCallback(
+    callback =>
+      Animated.sequence([
+        Animated.timing(colorAnim, {
+          toValue: 2,
+          duration: 100,
+          useNativeDriver: false,
+        }),
+        Animated.timing(colorAnim, {
+          toValue: 1,
+          duration: 200,
+          useNativeDriver: false,
+        }),
+      ]).start(() => callback()),
+    [colorAnim],
+  );
+
+  const pressInAnimation = useCallback(
+    () =>
+      Animated.timing(colorAnim, {
+        toValue: 1,
+        duration: 200,
+        useNativeDriver: false,
+        easing: Easing.ease,
+      }).start(),
+    [colorAnim],
+  );
+
+  const pressOutAnimation = useCallback(
+    () =>
+      Animated.timing(colorAnim, {
+        delay: 200,
+        toValue: 0,
+        duration: 200,
+        useNativeDriver: false,
+        easing: Easing.ease,
+      }).start(),
+    [colorAnim],
+  );
+
+  const {colorChangeStyling} = useMemo(
+    () => ({
+      colorChangeStyling: colorAnim.interpolate({
+        inputRange: [0, 1, 2],
+        outputRange: ['rgba(0,0,0,0)', 'rgba(0,0,0,.7)', 'rgba(255,0,0,.7)'],
+      }),
+    }),
+    [colorAnim],
+  );
+
+  return {
+    colorChangeStyling,
+    pressInAnimation,
+    pressOutAnimation,
+    longPressAnimation,
+  };
+};
