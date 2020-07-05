11b7d18e0a41d3b753dce2cb530425612fe2d48f
isberg1 <isberg1_github@outlook.com>
2020-07-05 12:11:14 +0200

    minor code improvements

--- Components/Item/ItemAmountSetter/ItemAmountSetter.js
+++ Components/Item/ItemAmountSetter/ItemAmountSetter.js
@@ -1,20 +1,7 @@
-import React, {useRef} from 'react';
+import React, {useRef, useCallback} from 'react';
 import {View, Text} from 'react-native';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 import {styles} from './styles';
-
-const swipingText = (add, disabled) =>
-  !disabled && (
-    <>
-      <View
-        style={[
-          styles.swipeView,
-          add ? styles.swipeViewLeft : styles.swipeViewRight,
-        ]}>
-        <Text style={styles.swipeText}>{add ? '+1' : '-1'} </Text>
-      </View>
-    </>
-  );
 
 // DOCUMENTATION: https://docs.swmansion.com/react-native-gesture-handler/docs/component-swipeable.html
 export const ItemAmountSetter = ({
@@ -25,14 +12,39 @@ export const ItemAmountSetter = ({
 }) => {
   const ref = useRef(null);
 
+  const swipingText = useCallback(
+    add => (
+      <>
+        <View
+          style={[
+            styles.swipeView,
+            add ? styles.swipeViewLeft : styles.swipeViewRight,
+          ]}>
+          <Text style={styles.swipeText}>{add ? '+1' : '-1'} </Text>
+        </View>
+      </>
+    ),
+    [],
+  );
+
+  const _onSwipeableWillOpen = useCallback(() => ref?.current?.close(), []);
+  const _renderLeftActions = useCallback(() => !disabled && swipingText(true), [
+    disabled,
+    swipingText,
+  ]);
+  const _renderRightActions = useCallback(
+    () => !disabled && swipingText(false),
+    [disabled, swipingText],
+  );
+
   return (
     <Swipeable
       ref={ref}
-      renderLeftActions={() => swipingText(true, disabled)}
-      renderRightActions={() => swipingText(false, disabled)}
+      renderLeftActions={_renderLeftActions}
+      renderRightActions={_renderRightActions}
       onSwipeableRightWillOpen={swipeLeft}
       onSwipeableLeftWillOpen={swipeRight}
-      onSwipeableWillOpen={() => ref?.current?.close()}>
+      onSwipeableWillOpen={_onSwipeableWillOpen}>
       {children}
     </Swipeable>
   );
