471ac8725c716290a7ac2345962f1e730182541d
isberg1 <isberg1_github@outlook.com>
2020-07-22 09:16:25 +0200

    deleted emitet js files after typescript compile

--- Components/List/styles.js
+++
@@ -1,8 +0,0 @@
-import {StyleSheet} from 'react-native';
-import {defaultBorder, gray, greenBlue, pink} from '../commonStyles';
-
-export const styles = StyleSheet.create({
-  scrollView: {
-    marginHorizontal: 30,
-  },
-});
